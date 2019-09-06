d52325d03588ac620d0dba851d578888e8f5ff02
aleesha-wood <spectrat@spectratart.com>
2019-09-06 19:07:33 -0400

    added fun bus yellow page y button easteregg

--- js/index.js
+++ js/index.js
@@ -6,3 +6,15 @@ for (var i = 0 ; i < signUpButtons.length; i++) {
     window.alert(`Vacation Starts Now!!`);
   });
 }
+
+//when 'Y' key is pressed, page background turns yellow (gold) and is returned to normal color when 'Y' key is released.
+window.addEventListener('keydown', (event) => {
+    if (event.keyCode == 89) {
+      document.body.style.backgroundColor = 'gold';
+    };
+});
+window.addEventListener('keyup', (event) => {
+  if (event.keyCode == 89) {
+    document.body.style.backgroundColor = 'white';
+  };
+});
