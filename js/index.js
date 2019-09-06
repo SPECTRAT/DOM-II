56d7a1b2d28d97c9b9caa472dea45c33be26a0ff
aleesha-wood <spectrat@spectratart.com>
2019-09-06 18:08:18 -0400

    added button click alert

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// window alerts above text when "Sign Me Up" buttons are pressed
+const signUpButtons = document.getElementsByClassName('btn');
+
+for (var i = 0 ; i < signUpButtons.length; i++) {
+  signUpButtons[i].addEventListener('click', (event) => {
+    window.alert(`Vacation Starts Now!!`);
+  });
+}
